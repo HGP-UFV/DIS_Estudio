29da0ef9b06ed284318599856cc4b44daed2b0c5
HGP <9004673@alumnos.ufv.es>
2022-09-25 16:20:51 +0200

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Variables
+        int edad;
+        String nombreConsola;
+
+        Scanner EscaneadorInt = new Scanner(System.in);
+        Scanner EscaneadorChar = new Scanner(System.in);
+
+
+
+        System.out.println("Hola como te llamas: ");
+        nombreConsola = EscaneadorChar.next();
+
+        System.out.println("\nCual es tu edad: ");
+        edad = EscaneadorInt.nextInt();
+
+        System.out.println("\nBienvenido/a " +nombreConsola +" su edad es " +edad);
+
+    }//main
+}//class main
